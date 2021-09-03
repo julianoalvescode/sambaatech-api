c57567732644bb3feb2025741ee32e6683b00599
Juliano Alves <contato.julianoalvess@gmail.com>
2021-09-03 17:20:11 -0300

    feat: 🎸 v0.6
    v0.6

--- src/server.ts
+++ src/server.ts
@@ -2,34 +2,28 @@ import express, { Response, Request } from "express";
 
 import axios from "axios";
 
-axios.create({
-	baseURL: "http://api.sambavideos.sambatech.com/v1",
-});
-
 const app = express();
+
+app.use(express.json());
 
 app.get("/medias/:mediaId", async (req: Request, res: Response) => {
 	const { mediaId } = req.params;
 
-	// const { data } = await axios
-	// 	.get(`/medias/${mediaId}`, {
-	// 		params: {
-	// 			access_token: "d2c6ebae3cdc354c80121cf034de7a2b",
-	// 			pid: "",
-	// 		},
-	// 	})
-	// 	.then((r) => {
-	// 		const status = r.status;
-
-	// 		if (status === 200) {
-	// 			return res.json(JSON.parse(data)).status(200);
-	// 		}
-
-	// 		return res.json({ message: "Error" }).status(400);
-	// 	});
-	return res.json({ message: "Hello World" });
+	await axios
+		.get(`http://api.sambavideos.sambatech.com/v1/medias/${mediaId}`, {
+			params: {
+				access_token: process.env.ACESS_TOKEN,
+				pid: process.env.PID_CODE,
+			},
+		})
+		.then(({ data }) => {
+			return res.json(data).status(200);
+		})
+		.catch((err) => {
+			return res.status(400).json({ message: err.message });
+		});
 });
 
 app.listen(process.env.PORT | 3333, () => {
-	console.log(`API is running }`);
+	console.log(`API is running`);
 });
